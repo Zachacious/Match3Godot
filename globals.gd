4b25c20a2313fe75b8203884c88ce8f122328dad
zachacious <back4moore@gmail.com>
2022-07-16 10:00:04 -0500

    added monster graphics

--- globals.gd
+++ globals.gd
@@ -5,10 +5,18 @@ var grid = {
 	'coords': {}
 }
 
+#var blocks =  [
+#	{'name':'red', 'texture': preload("res://sprites/red.png")},
+#	{'name':'blue', 'texture':preload("res://sprites/blue.png")},
+#	{'name':'green', 'texture':preload("res://sprites/green.png")},
+#	{'name':'magenta', 'texture':preload("res://sprites/magenta.png")},
+#	{'name':'seaweed','texture':preload("res://sprites/seaweed.png")},
+#]
+
 var blocks =  [
-	{'name':'red', 'texture': preload("res://sprites/red.png")},
-	{'name':'blue', 'texture':preload("res://sprites/blue.png")},
-	{'name':'green', 'texture':preload("res://sprites/green.png")},
-	{'name':'magenta', 'texture':preload("res://sprites/magenta.png")},
-	{'name':'seaweed','texture':preload("res://sprites/seaweed.png")},
+	{'name':'cat', 'texture': preload("res://sprites/monsters/cat.png")},
+	{'name':'lizard', 'texture':preload("res://sprites/monsters/lizard.png")},
+	{'name':'owl', 'texture':preload("res://sprites/monsters/owl.png")},
+	{'name':'pig', 'texture':preload("res://sprites/monsters/pig.png")},
+	{'name':'rabbit','texture':preload("res://sprites/monsters/rabbit.png")},
 ]
